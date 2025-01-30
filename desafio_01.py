4f2d60d301fbe56546cd63eaf9542cd68d67588e
Renan Darros Miranda <renan.dmiranda@hsl.org.br>
2025-01-30 14:20:22 -0300

    Desafio 01

---
+++ desafio_01.py
@@ -0,0 +1,6 @@
+a = input("Digite seu nome: ")
+b = float(input("Digite seu salario: "))
+c = float(input("Digite o numero do seu bonus: "))
+kpy = 1000 + b * c
+
+print(f"{a} seu bonus será de R${kpy}")
